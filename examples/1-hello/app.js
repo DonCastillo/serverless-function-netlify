4fba062b8cc552370215a9aacdf12306cbe38745
DonCastillo <don.qcastillo@yahoo.com>
2021-06-21 00:14:24 -0600

    modified example 1 app.js

---
+++ examples/1-hello/app.js
@@ -0,0 +1,12 @@
+const result = document.querySelector('.result');
+
+const fetchData = async () => {
+    try {
+        const {data} = await axios.get('/.netlify/functions/hello');
+        result.textContent = data;
+    } catch (error) {
+        result.textContent = error.response.data;
+    }
+}
+
+fetchData();
--- functions/hello.js
+++ functions/hello.js
@@ -1,7 +1,6 @@
 // can be accessed via domain/.netlify/functions/hello
 
 exports.handler = async (event, context) => {
-    console.log(context)
     return {
         statusCode: 200,
         body: 'Our First Netlify Function Example'
