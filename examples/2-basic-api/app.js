b37e47283efb994cbed800b3b2c337bb25569536
DonCastillo <don.qcastillo@yahoo.com>
2021-06-24 22:16:35 -0600

    display item cards

--- examples/2-basic-api/app.js
+++ examples/2-basic-api/app.js
@@ -1,1 +1,29 @@
-const result = document.querySelector('.result')
+// const { default: axios } = require("axios");
+
+const result = document.querySelector('.result');
+
+const fetchData = async () => {
+    console.log('second example');
+    try {
+        const {data} = await axios.get('/api/basic-api');
+        const products = data.map(product => {
+            const {image: {url}, name, price} = product;
+            return `
+                <article class="product">
+                    <img src="${url}" alt="${name}"/>
+                    <div class="info">
+                        <h5>${name}</h5>
+                        <h5 class="price">$${price}</h5>
+                    </div>
+                </article>
+            `;
+        }).join('')
+        // console.log(products);
+        result.innerHTML = products;
+    } catch (error) {
+        result.innerHTML = `<h4>There was an error. Please try again later.</h4>`;
+    }
+}
+
+
+fetchData();
