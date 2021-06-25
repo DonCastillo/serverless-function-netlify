eb9e8e74c8dc3e2d35bed656de0abb6ff8003bed
DonCastillo <don.qcastillo@yahoo.com>
2021-06-24 21:24:21 -0600

    learned redirect

--- examples/1-hello/app.js
+++ examples/1-hello/app.js
@@ -2,7 +2,7 @@ const result = document.querySelector('.result');
 
 const fetchData = async () => {
     try {
-        const {data} = await axios.get('/.netlify/functions/hello');
+        const {data} = await axios.get('/api/hello');
         result.textContent = data;
     } catch (error) {
         result.textContent = error.response.data;
